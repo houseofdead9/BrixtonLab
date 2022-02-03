1a90862f401f687245b813cb28ef19b13e169f13
muhammadzeehlx <94970989+muhammadzeehlx@users.noreply.github.com>
2022-02-04 03:09:11 +0500

    Add project files.

---
+++ Controllers/RequisitionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrixtonLab.Controllers
+{
+    public class RequisitionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult NewIdLisReq()
+        {
+            return View();
+        }
+        public IActionResult IdReqReport()
+        {
+            return View();
+        }
+        public IActionResult BloodReqLis()
+        {
+            return View();
+        }
+    }
+}
