f765e5769d6ea18682d7bff6700ea228b3129515
muhammadzeehlx <94970989+muhammadzeehlx@users.noreply.github.com>
2022-05-21 05:07:15 +0500

    patient module implementation

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrixtonLab.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PatientProfile()
+        {
+            return View();
+        }
+    }
+}
